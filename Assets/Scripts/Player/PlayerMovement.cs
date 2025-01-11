3fe36c0593f9344512b5cee7e6629fdb76102c95
Awais7848 <40107360+Awais7848@users.noreply.github.com>
2025-01-11 15:08:54 +0500

    Player Controller & Animator Setup
    - Animator Setup (Run , Jump , Slide)
    - Jump Addition To Controller
    - Slide Addition to Controller

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,12 +11,41 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 positionV;
 
-   [SerializeField] float Smoothness;
+    [SerializeField] float Smoothness;
+
+    [SerializeField]Animator animator;
+
+    [SerializeField] float height;
+
+    [SerializeField] LayerMask groundCheck;
+    [SerializeField] float GroundDistance;
+    [SerializeField] Transform groundCheckPosition;
+    [SerializeField] CapsuleCollider capsuleCollider;
+   bool IsGrounded
+    {
+
+        get
+        {
+           if(Physics.Raycast(groundCheckPosition.position, -groundCheckPosition.up, GroundDistance, groundCheck))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+    }
+
     private void Awake()
     {
         playerRB = GetComponent<Rigidbody>();
     }
-  
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,9 +72,32 @@ public class PlayerMovement : MonoBehaviour
         positionV.z = playerRB.position.z;
         positionV.y = transform.position.y;
         transform.position = Vector3.Lerp(transform.position,positionV,Time.deltaTime*Smoothness);
+        animator.SetFloat("Speed", playerRB.velocity.magnitude);
+        if (Input.GetKeyDown(KeyCode.Space)&&IsGrounded)
+        {
+            Jump();
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
 
-        
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            capsuleCollider.height = 0.5f;
+            capsuleCollider.center= new Vector3(0,0.3f,0f);
+            animator.SetBool("Slide", true);
+        }
+
+     
+
+        Debug.Log("Is Grounded : " + IsGrounded);
     }
+
+
+    
+
 
     private void FixedUpdate()
     {
@@ -53,4 +105,16 @@ public class PlayerMovement : MonoBehaviour
 
 
     }
+
+   
+
+    public void Jump()
+    {
+        playerRB.velocity += new Vector3(0, Mathf.Sqrt(-2.0f * Physics2D.gravity.y * height),0);
+        animator.SetBool("Jump", true);
+        animator.SetBool("Slide", false);
+
+    }
+
+
 }
